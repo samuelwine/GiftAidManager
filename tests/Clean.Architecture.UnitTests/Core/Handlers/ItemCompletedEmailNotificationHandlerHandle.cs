e98403fb84a04fb0c2d85130fc0c27a27100b630
shmuel <samuelwine@gmail.com>
2021-06-07 20:26:48 +0100

    Initial commit

---
+++ tests/Clean.Architecture.UnitTests/Core/Handlers/ItemCompletedEmailNotificationHandlerHandle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Clean.Architecture.Core.ProjectAggregate;
+using Clean.Architecture.Core.Interfaces;
+using Moq;
+using Xunit;
+using Clean.Architecture.Core.ProjectAggregate.Handlers;
+using Clean.Architecture.Core.ProjectAggregate.Events;
+
+namespace Clean.Architecture.UnitTests.Core.Handlers
+{
+    public class ItemCompletedEmailNotificationHandlerHandle
+    {
+        private ItemCompletedEmailNotificationHandler _handler;
+        private Mock<IEmailSender> _emailSenderMock;
+
+        public ItemCompletedEmailNotificationHandlerHandle()
+        {
+            _emailSenderMock = new Mock<IEmailSender>();
+            _handler = new ItemCompletedEmailNotificationHandler(_emailSenderMock.Object);
+        }
+
+        [Fact]
+        public async Task ThrowsExceptionGivenNullEventArgument()
+        {
+            Exception ex = await Assert.ThrowsAsync<ArgumentNullException>(() => _handler.Handle(null, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task SendsEmailGivenEventInstance()
+        {
+            await _handler.Handle(new ToDoItemCompletedEvent(new ToDoItem()), CancellationToken.None);
+
+            _emailSenderMock.Verify(sender => sender.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
