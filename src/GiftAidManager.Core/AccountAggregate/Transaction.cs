11a4abd5b87b451df9a6d2b8ef4cc573c4e50fde
shmuel <samuelwine@gmail.com>
2021-06-15 22:11:41 +0100

    Add entities

---
+++ src/GiftAidManager.Core/AccountAggregate/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using GiftAidManager.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiftAidManager.Core.AccountAggregate
+{
+    public class Transaction : BaseEntity
+    {
+        public Customer Customer { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public int Value { get; set; }
+    }
+}
